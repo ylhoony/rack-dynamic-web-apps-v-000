76e81d526c7262de08e22e2fe614d39e73dbf83d
Hoon Lee <yl.hoony@gmail.com>
2018-01-02 02:12:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,13 @@
+# class Application
+# 
+#   def call(env)
+#     resp = Rack::Response.new
+#     resp.write "Hello, World"
+#     resp.finish
+#   end
+# 
+# end
+
 class Application
  
   def call(env)
